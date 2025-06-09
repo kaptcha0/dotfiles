f1ea1419255955a6f05fc332a0abe12ca96f8652
kaptcha0 <50426223+kaptcha0@users.noreply.github.com>
2025-06-09 14:37:58 -0400

    updated all dots

---
+++ linux-darwin/home-manager/install.sh
@@ -0,0 +1,4 @@
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+home-manager switch --flake .#$(whoami)
